609cc0d49b792e3a954b9c6d8fd17904d28dfe98
Aaron Turner <aaron@aaronthedev.com>
2018-05-20 15:28:32 -0700

    Added WasmBoy and aesthetic-css

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
+// Our root component
 import "./index.scss";
 import { Component } from "preact";
+import { WasmBoy } from "wasmboy";
 
 export default class App extends Component {
   componentDidMount() {
     document.addEventListener("deviceready", () => {
       console.log("Cordova Launched Device Ready!");
     });
+    console.log("WasmBoy:", WasmBoy);
   }
 
   render() {
     return (
       <div>
         <h1>Hello, World!</h1>
+        <div class="aesthetic-windows-95-button">
+          <button>OK</button>
+        </div>
       </div>
     );
   }
