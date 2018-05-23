726970ee59b8d564c1c4fe54bec69dd7107361a8
Aaron Turner <aaron@aaronthedev.com>
2018-05-22 20:57:42 -0700

    Added a wasmboy ready check

--- src/components/wasmboyCanvas/wasmboyCanvas.js
+++ src/components/wasmboyCanvas/wasmboyCanvas.js
@@ -4,18 +4,22 @@ import { WasmBoy } from "wasmboy";
 
 export default class WasmBoyCanvas extends Component {
   componentDidMount() {
-    // Get our HTML5 Canvas element
-    const canvasElement = document.querySelector("#wasmboy-canvas");
+    // Check if we are already ready and initialized
+    // (this is to avoid resetting a game on layout changes)
+    if (!WasmBoy.isReady()) {
+      // Get our HTML5 Canvas element
+      const canvasElement = document.querySelector("#wasmboy-canvas");
 
-    const wasmboyInitTask = async () => {
-      await WasmBoy.config({}, canvasElement);
-      console.log("WasmBoy is configured!");
-      await WasmBoy.loadROM("assets/openSourceGames/tobutobugirl.gb");
-      console.log("TobuTobuGirl loaded!");
-      await WasmBoy.play();
-    };
+      const wasmboyInitTask = async () => {
+        await WasmBoy.config({}, canvasElement);
+        console.log("WasmBoy is configured!");
+        await WasmBoy.loadROM("assets/openSourceGames/tobutobugirl.gb");
+        console.log("TobuTobuGirl loaded!");
+        await WasmBoy.play();
+      };
 
-    wasmboyInitTask();
+      wasmboyInitTask();
+    }
   }
 
   render() {
