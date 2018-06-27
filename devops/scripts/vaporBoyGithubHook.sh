a52e6e36b310b7bd57aa52030060097b6e5c9243
Aaron Turner <aaron@aaronthedev.com>
2018-06-26 23:18:10 -0700

    Updated github hook service

--- devops/scripts/vaporBoyGithubHook.sh
+++ devops/scripts/vaporBoyGithubHook.sh
@@ -5,4 +5,4 @@ source .env
 
 echo "Using the Github Hook Secret: $GITHUB_HOOK_STAGING"
 
-githubhook --callback=/gitmasterpr --secret=$GITHUB_HOOK_STAGING push:vaporBoy:refs/heads/master ./buildVaporBoy.sh
+githubhook --port=9081 --callback=/gitmasterpr --secret=$GITHUB_HOOK_STAGING push:vaporBoy:refs/heads/master ./buildVaporBoy.sh
