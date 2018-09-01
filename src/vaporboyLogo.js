c9a686d59d9b45ab664eefd08e58d77fe700a065
Aaron Turner <aaron@aaronthedev.com>
2018-09-01 02:21:31 -0700

    Fixed up some of the PWA score

--- src/vaporboyLogo.js
+++ src/vaporboyLogo.js
@@ -1,10 +1,6 @@
 // File to return a vaporboy logo src
 
-const VAPORBOYS = [
-  "vaporboyarizona.png",
-  "vaporboybluebeach.png",
-  "vaporboyvhs.png"
-];
+const VAPORBOYS = ["vaporboyarizona", "vaporboybluebeach", "vaporboyvhs"];
 
 let vaporboyLogo = undefined;
 
@@ -16,6 +12,6 @@ export const getVaporBoyLogo = () => {
   // Get a vaporboy
   const selectedVaporBoy =
     VAPORBOYS[Math.floor(Math.random() * VAPORBOYS.length)];
-  vaporboyLogo = `/assets/vaporboy/512/${selectedVaporBoy}`;
+  vaporboyLogo = `/assets/vaporboy/512/${selectedVaporBoy}.png`;
   return vaporboyLogo;
 };
