a92cdf1fba0151fa7ab7d5d05096512e3b74f0c9
Aaron Turner <aaron@aaronthedev.com>
2019-06-16 16:19:53 -0700

    Removed some devops-y things that are no lnger needed

--- devops/scripts/vaporBoyGithubHook.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Source our env file
-source .env
-
-echo "Using the Github Hook Secret: $GITHUB_HOOK_STAGING"
-
-githubhook --port=9081 --callback=/gitmasterpr --secret=$GITHUB_HOOK_STAGING push:vaporBoy:refs/heads/master ./buildVaporBoy.sh
